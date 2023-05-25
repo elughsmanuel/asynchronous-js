20b3ed62a0e004107ac51b462534acdb57f4f5c7
Emmanuel Elugbaju <elugbajuemmanueltobi@gmail.com>
2023-05-25 01:47:04 +0100

    Async Await

--- index.js
+++ index.js
@@ -19,6 +19,50 @@ const writeFilePro = (file, data) => {
   });
 };
 
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
+    console.log(`Breed: ${data}`);
+
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/labrador/images/random`
+    );
+    console.log(res.body.message);
+
+    await writeFilePro('dog-img.txt', res.body.message);
+    console.log('Random dog image saved to file!');
+  } catch (err) {
+    console.log(err);
+
+    throw err;
+  }
+  return '2: READY :)';
+};
+
+(async () => {
+  try {
+    console.log('1: Will get the dog pics!');
+    const x = await getDogPic();
+    console.log(x);
+    console.log('3: Done getting dog pics!');
+  } catch (err) {
+    console.log('ERROR *');
+  }
+})();
+
+/*
+console.log('1: Will get dog pics!');
+getDogPic()
+  .then(x => {
+    console.log(x);
+    console.log('3: Done getting dog pics!');
+  })
+  .catch(err => {
+    console.log('ERROR *');
+  });
+*/
+
+/*
 readFilePro(`${__dirname}/dog.txt`)
   .then(data => {
     console.log(`Breed: ${data}`);
@@ -34,3 +78,4 @@ readFilePro(`${__dirname}/dog.txt`)
   .catch(err => {
     console.log(err.message);
   });
+*/
